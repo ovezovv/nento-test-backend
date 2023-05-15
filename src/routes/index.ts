04d84da1cba702d8ab752fa4b27bd563077e0286
meylis <ovezovm7@gmail.com>
2023-05-15 11:03:49 -0700

    initial

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,11 @@
-import { Router } from "express";
+import { Request, Router, Response } from "express";
 import { authRouter } from "./auth";
 
 export const router: Router = Router();
 
+router.use("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    data: "Welcome to Nento Instagram Scraper!"
+  })
+})
 router.use('/auth', authRouter);
